daea9737b22ed2d54d8ae33bb5f2e21a746ce102
Berat Cevik <berat@auvitas.com>
2020-04-13 18:13:47 +0300

    Update pods

--- The Real AR/Model/Scene.swift
+++ The Real AR/Model/Scene.swift
@@ -1,15 +1,9 @@
 
-import Foundation
-
 class Scene {
-    
-    // MARK: - Variables
-	
 	var name: String
 	var steps: [Step]
     
     // MARK: - Initialization
-	
 	init?(name: String, steps: [Step]) {
 		self.name = name
 		self.steps = steps
